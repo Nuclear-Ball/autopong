d09efdc57e99eee9568906f520783e714226f3c8
Nuclear Ball <66165153+Nuclear-Ball@users.noreply.github.com>
2021-02-05 13:48:36 +0300

    Add files via upload

--- pong.py
+++ pong.py
@@ -6,11 +6,13 @@ pygame.font.init()
 font = pygame.font.Font('freesansbold.ttf', 20)
 lose = font.render('Игрок 2 ВЫИГРАЛ!', True, WHITE)
 win = font.render('Игрок 1 ВЫИГРАЛ!', True, WHITE)
-screen = pygame.display.set_mode((320, 224))
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("Pong")
 pygame.display.set_icon(pygame.image.load('layers/pong.png'))
-x = int(0)
+x = int(farness + paddle_height * 2 + ball_size)
 y = int(0)
+screen_size_x, screen_size_y = pygame.display.get_window_size()
+print(screen_size_y)
 paddle_y = int(0)
 enemy_paddle_y = int(0)
 down = bool(1)
@@ -28,15 +30,15 @@ score_p2 = 0
 
 
 def paddle_ai():
-    global enemy_paddle_y, enemy_paddle_up, enemy_paddle_down, paddle_down, paddle_up
-    if x < W // 2:
+    global enemy_paddle_up, enemy_paddle_down, paddle_up, paddle_down
+    if x < screen_size_x // 2:
         if y > enemy_paddle_y + paddle_width // 2:
             enemy_paddle_down = 1
             enemy_paddle_up = 0
         elif y < enemy_paddle_y + paddle_width // 2:
             enemy_paddle_down = 0
             enemy_paddle_up = 1
-    if x > W // 2:
+    if x > screen_size_x // 2:
         if y > paddle_y + paddle_width // 2:
             paddle_down = 1
             paddle_up = 0
@@ -46,47 +48,52 @@ def paddle_ai():
 
 
 def main():
-    global x, y, up, down, left, right, paddle_down, paddle_up, paddle_y, enemy_paddle_y, enemy_paddle_up, enemy_paddle_down, lose_state, win_state, score_p1, score_p2
+    global x, y, up, down, left, right, paddle_y, paddle_up, paddle_down, enemy_paddle_y, enemy_paddle_up, enemy_paddle_down, lose_state, win_state, score_p1, score_p2
     score1 = font.render(f'Счет (Игрок 1): {score_p1}', True, WHITE)
-    score2 = font.render(f'Счет (Игрок 2): {score_p2 - 5}', True, WHITE)
+    score2 = font.render(f'Счет (Игрок 2): {score_p2}', True, WHITE)
     key = pygame.key.get_pressed()
-    y += down * speed - up * speed
-    x += right * speed - left * speed
     paddle_ai()
+    '''Paddle collision'''
     if enemy_paddle_y < 0:
         enemy_paddle_y = 0
-    if (enemy_paddle_y + paddle_width) > H:
-        enemy_paddle_y = H - paddle_width
+    if (enemy_paddle_y + paddle_width) > screen_size_y:
+        enemy_paddle_y = screen_size_y - paddle_width
     if paddle_y < 0:
         paddle_y = 0
-    if (paddle_y + paddle_width) > H:
-        paddle_y = H - paddle_width
-    if x >= W - paddle_height and paddle_y < y < paddle_y + paddle_width:
+    if (paddle_y + paddle_width) > screen_size_y:
+        paddle_y = screen_size_y - paddle_width
+    '''Detect collision'''
+    if screen_size_x - farness - paddle_height <= x + ball_size <= screen_size_x - farness and paddle_y < y + ball_size // 2 < paddle_y + paddle_width:
         right = 0
         left = 1
         score_p1 += 1
-    if x <= paddle_height and enemy_paddle_y < y < enemy_paddle_y + paddle_width:
+    if farness + paddle_height >= x - ball_size >= farness and enemy_paddle_y < y + ball_size // 2 < enemy_paddle_y + paddle_width:
         right = 1
         left = 0
         score_p2 += 1
-    pygame.draw.circle(screen, WHITE, (x, y), 5)
+    '''Move objects'''
+    y += down * speed - up * speed
+    x += right * speed - left * speed
     paddle_y += paddle_down * speed - paddle_up * speed
     enemy_paddle_y += enemy_paddle_down * (speed - 0) - enemy_paddle_up * (speed - 0)
-    pygame.draw.rect(screen, WHITE, (310, paddle_y, paddle_height, paddle_width))
-    pygame.draw.rect(screen, WHITE, (0, enemy_paddle_y, paddle_height, paddle_width))
+    '''Drawing'''
+    pygame.draw.circle(screen, WHITE, (x, y), ball_size)
+    pygame.draw.rect(screen, WHITE, ((screen_size_x - farness) - paddle_height, paddle_y, paddle_height, paddle_width))
+    pygame.draw.rect(screen, WHITE, (farness, enemy_paddle_y, paddle_height, paddle_width))
+    '''Stop moving'''
     paddle_up = 0
     paddle_down = 0
     enemy_paddle_up = 0
     enemy_paddle_down = 0
-    if y >= H - paddle_height:
+    if y >= screen_size_y - ball_size:
         up = 1
         down = 0
-    if y <= 0:
+    if y <= 0 + ball_size:
         up = 0
         down = 1
     if x <= -10:
         win_state = True
-    if x >= W:
+    if x >= screen_size_x:
         lose_state = True
 
     if key[pygame.K_w]:
@@ -102,8 +109,7 @@ def main():
     if key[pygame.K_DOWN]:
         enemy_paddle_up = 0
         enemy_paddle_down = 1
-
-    pygame.time.delay(10)
+    pygame.time.delay(0)
 
     if win_state:
         screen.blit(win, (20, 0))
